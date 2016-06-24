a9f8634600f6bb3c8abb3ab8b14833f93659d941
jamescastle <mailmirmalw@web.de>
2016-06-24 16:49:29 +0000

    Add files via upload

---
+++ ger.ts
@@ -0,0 +1,276 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_GB">
+<context>
+    <name>DsoFinder</name>
+    <message>
+        <location filename="../dsofinder.ui" line="85"/>
+        <source>Take Screen</source>
+        <translation>Screenshot</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="104"/>
+        <location filename="../ui_functions.cpp" line="129"/>
+        <source>Find Objects</source>
+        <translation>Suche</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="155"/>
+        <source>Discovered:</source>
+        <translation>Gefunden:</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="247"/>
+        <source>Pixel-Finder</source>
+        <translation>Pixelsuche</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="271"/>
+        <source>using footprints of 29 items
+(events till EM 2016 included)</source>
+        <translation>nutze Umrisse von 29 Sammlerstücken
+(Events bis EM 2016)</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="282"/>
+        <source>Save Screen</source>
+        <translation>Als Referenzbild speichern</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="289"/>
+        <source>Load Screen</source>
+        <translation>Referenzbild laden</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="297"/>
+        <source>Building-Site-Finder</source>
+        <translation>Differenzsuche</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="351"/>
+        <source>Show BG</source>
+        <translation>Referenzbild anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="371"/>
+        <source>Edit caption</source>
+        <translation>Benennung ändern</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="385"/>
+        <source>Delete BG</source>
+        <translation>Referenzbild löschen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="407"/>
+        <source>Save as BG</source>
+        <translation>Als Referenzbild speichern</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="443"/>
+        <source>onTop</source>
+        <translation>im Vordergrund</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="504"/>
+        <source>Behavior / Multimonitor</source>
+        <translation>Verhalten / Multimonitor</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="541"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Check this, if you want to use half-sized grafics&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Diesen Haken setzen, wenn Grafiken mit halber Größe im Spiel genutzt werden.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="544"/>
+        <source>Use halfisize</source>
+        <translation>Grafiken in halber Größe</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="559"/>
+        <source>Halfsized grafics</source>
+        <translation>Nutze Grafiken in halber Größe</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="586"/>
+        <source>Take Screen Hit</source>
+        <translation>&quot;Screenshot&quot; angeklickt</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="592"/>
+        <source>Jump to previous position</source>
+        <translation>Springe auf vorherige Position</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="599"/>
+        <source>Stay big</source>
+        <translation>Bleibe maximiert</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="644"/>
+        <source>Performance / Appearance</source>
+        <translation>Leistung / Darstellung</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="663"/>
+        <source>Include Items</source>
+        <translation>Zu suchende Sammlerstücke</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="669"/>
+        <source>Basic</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="676"/>
+        <source>Adventure</source>
+        <translation>Abenteuer</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="689"/>
+        <source>Choose event:</source>
+        <translation>Wähle Event:</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="733"/>
+        <source>Take Screen Waittime for Resize</source>
+        <translation>Wartezeit vor Größenänderung</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="827"/>
+        <source>Searchtimer</source>
+        <translation>Suchzeit</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="836"/>
+        <source>Show</source>
+        <translation>Anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="871"/>
+        <source>Colors</source>
+        <translation>Farben</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="889"/>
+        <source>Std.:</source>
+        <translation>Std.:</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="950"/>
+        <source>Event:</source>
+        <translation>Event:</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="990"/>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="1034"/>
+        <source>Options</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="1144"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.ui" line="1183"/>
+        <source>!!Zoom out as far as possible!!</source>
+        <translation>!! So weit herauszoomen wie möglich !!</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="290"/>
+        <source>Background file has wrong resolution!</source>
+        <translation>Das Referenzbild hat die falsche Auflösung!</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="295"/>
+        <location filename="../dsofinder.cpp" line="413"/>
+        <source>No valid background file.</source>
+        <translation>Kein gültiges Referenzbild.</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="387"/>
+        <source>Main Island S5</source>
+        <translation>Hauptinsel S5</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="388"/>
+        <source>Save in Slot %1</source>
+        <translation>In Slot %1 speichern</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="388"/>
+        <source>Enter caption:</source>
+        <translation>Beschreibung:</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="397"/>
+        <source>Confirm overwrite</source>
+        <translation>Überschreiben bestätigen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="397"/>
+        <source>Do you really want to replace the background in Slot %1?</source>
+        <translation>Willst du das Referenzbild in Slot %1 wirklich überschreiben?</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="422"/>
+        <source>Edit Slot %1</source>
+        <translation>Bearbeite Slot %1</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="422"/>
+        <source>Enter new caption:</source>
+        <translation>Neue Beschreibung eingeben:</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="432"/>
+        <source>Delete Background</source>
+        <translation>Referenzbild löschen</translation>
+    </message>
+    <message>
+        <location filename="../dsofinder.cpp" line="432"/>
+        <source>Do you really want to delete the background file in Slot %1?</source>
+        <translation>Willst du das Referenzbild in Slot %1 wirklich löschen?</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="70"/>
+        <source>+: New Slot</source>
+        <translation>+: Neuer Slot</translation>
+    </message>
+    <message>
+        <location filename="../ui_functions.cpp" line="108"/>
+        <source>No background file found! Create it like this:
+a) Open DSO in your max-sized browser.
+b) Make sure there is no collectible left on your map at the moment
+c) Zoom out as far as possible
+d) Move to a fixed position(e.g.: hitting 5 on your keyboard)
+e) Grab a flowerbed or other 1x1 decorational item from you starmenu (Most of the map should apear greenish now)
+f) Open this program, move the window somewhere in your screen where it only hides water
+g) Hit Take Screen
+h) Hit Save as BG
+
+!Redo this everytime you move/erase/build new stuff on your map!</source>
+        <translation>Kein Referenzbild gefunden! Erstelle es folgendermaßen:
+a)	Maximiere DSO im Browser
+b)	Stelle sicher, dass momentan KEIN Sammlerstück auf der Insel ist
+c)	Zoome so weit heraus, wie möglich
+d)	Springe zu einer festen Position (z.B. die &apos;5&apos; auf der Tastatur tippen)
+e)	Wähle ein Blumenbeet oder ein anderes Dekoelement mit 1x1 aus deinem Sternmenü aus (Die Insel sollte jetzt grünlich sein)
+f)	Wähle den DSO Combofinder aus und bewege das Fenster dort hin, wo es nur Wasser verdeckt
+g)	Klicke auf &quot;Screenshot&quot;
+h)	Klicke auf &quot;Referenzbild speichern&quot;
+
+Wiederhole das jedes mal, wenn du ein Gebäude bewegst/abreißt oder neu baust!</translation>
+    </message>
+    <message>
+        <location filename="../ui_functions.cpp" line="125"/>
+        <source>searching...</source>
+        <translation>Suche...</translation>
+    </message>
+</context>
+</TS>
